8f8f534d064ff77820c52d6f1f3a3f40bedd0227
TimNet-codx <jesulayomitimze55@gmail.com>
2025-03-12 11:03:39 +0100

    FontEnd Fix

--- server.js
+++ server.js
@@ -52,7 +52,12 @@ if (process.env.NODE_ENV === 'development') {
 //     res.json({ msg: 'test route' });
 //   });
  // Public and Upload
- app.use(express.static(path.resolve(__dirname, './public')));
+
+ // For Developing
+//  app.use(express.static(path.resolve(__dirname, './public')));
+ // Replace with this for production
+ app.use(express.static(path.resolve(__dirname, './client/dist')));
+
   app.use(cookieParser());
   app.use(express.json());
   //app.use(morgan('dev'));
@@ -64,10 +69,15 @@ app.use('/api/v1/jobs',authenticateUser ,jobRouter);
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', authenticateUser, userRouter);
 
-// Access Point
+// Access Point For Developing
+// app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, './public/assets', 'index.html'));
+// });
+// Access Point Replace with this for production
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './public/assets', 'index.html'));
+  res.sendFile(path.resolve(__dirname, './client/dist', 'index.html'));
 });
+
 
  
 
